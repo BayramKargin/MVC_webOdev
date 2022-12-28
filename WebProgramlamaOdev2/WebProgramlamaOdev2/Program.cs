c6944682c5204a8a837271dd9871a28faf54bbdb
BayramKargin <bayram.kargin@ogr.sakarya.edu.tr>
2022-12-28 13:06:23 +0300

    Login

--- WebProgramlamaOdev2/WebProgramlamaOdev2/Program.cs
+++ WebProgramlamaOdev2/WebProgramlamaOdev2/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -17,6 +18,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
